de6a1d02760b28aef502147b27dd70da387744e3
Armin Fröhlich <mail@arminfroehlich.de>
2021-10-08 16:14:06 +0200

    fix type definition of link_preview_generator
    `puppeteerArgs` and `puppeteerAgent` have defaults
    
    `executablePath` is accessed behind an if
    
    return value is an async function -> `Promise<..>`

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,10 @@ export = link_preview_generator;
 
 declare function link_preview_generator(
     uri: string,
-    puppeteerArgs: string[],
-    puppeteerAgent: string,
-    executablePath: string
-): LinkPreviewResult;
+    puppeteerArgs?: string[],
+    puppeteerAgent?: string,
+    executablePath?: string
+): Promise<LinkPreviewResult>;
 
 declare interface LinkPreviewResult {
     title: string;
